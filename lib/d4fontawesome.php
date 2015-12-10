159ba5b22c7b0544572f265e2c9c2164920198bf
MerrittofD4 <merritt@d4webdesign.com>
2015-12-10 10:25:47 -0700

    Updated script and style URLs

--- lib/d4fontawesome.php
+++ lib/d4fontawesome.php
@@ -9,9 +9,9 @@ add_action( 'wp_enqueue_scripts', 'register_plugin_elements' );
  * Register style sheet.
  */
 function register_plugin_elements() {
-	wp_register_style( 'd4fontawesome', plugins_url( 'FontAwesome/css/font-awesome.min.css' ) );
+	wp_register_style( 'd4fontawesome', plugins_url( '../css/font-awesome.min.css' , __FILE__ ) );
 	wp_enqueue_style( 'd4fontawesome' );
-	wp_register_script( 'd4fontawesome', plugins_url( 'FontAwesome/js/d4fontawesome.js'), array( 'jquery' ), 'v20131005', true );
+	wp_register_script( 'd4fontawesome', plugins_url( '../js/d4fontawesome.js' , __FILE__ ), array( 'jquery' ), 'v20131005', true );
 	wp_enqueue_script('d4fontawesome');
 }
 
